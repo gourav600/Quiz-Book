3ebc474cc4b9f5b035944dc6b9ee88e621f39eb7
gourav600 <37892581+gourav600@users.noreply.github.com>
2019-01-11 15:59:59 +0530

    Create play.py

---
+++ play.py
@@ -0,0 +1,8 @@
+import discord
+from discord.ext import commands
+from discord.ext commands import Bot
+import asyncio
+import random
+import request
+import os
+bot token:client.run(str(os.environ.get('Bot_Token')))
