32421f98d4555a11ccb02a6f8d33205d60c19c6c
gourav600 <37892581+gourav600@users.noreply.github.com>
2019-06-24 23:01:24 +0530

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord.ext import commands
-from discord.ext commands import Bot
-import asyncio
-import random
-import request
-import os
-bot token:client.run(str(os.environ.get('Bot_Token')))
